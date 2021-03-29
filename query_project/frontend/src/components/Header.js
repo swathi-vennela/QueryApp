9f38b75674322d7d3e072364a2e6fc78b9ce2fea
swathi-vennela <vennela.2k1@gmail.com>
2021-03-29 11:28:44 +0530

    .git of frontend removed

---
+++ query_project/frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <h1>Student Details</h1>
+        <hr />
+        <h5>
+           {/* <i>presents</i>  */}
+        </h5>
+        <h1>App with React + Django</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
